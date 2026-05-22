306dcbb9a5bf1ae59da0a5956c45185ffdb24103
Remo Delfino <remonicolas@gmail.com>
2026-05-22 00:28:59 -0300

    firebase con api

--- src/components/PushInit.tsx
+++ src/components/PushInit.tsx
@@ -1,17 +1,39 @@
 'use client'
 
 import { useEffect } from 'react'
+import { getToken } from 'firebase/messaging'
+import { messaging } from '@/lib/firebase/firebase'
 
 export default function PushInit() {
   useEffect(() => {
-    console.log('🔥 PushInit mounted')
+    async function initPush() {
+      console.log('🔥 PushInit mounted')
 
-    if (!('Notification' in window)) return
-    if (!navigator.serviceWorker) return
+      if (!('Notification' in window)) return
+      if (!navigator.serviceWorker) return
+      if (!messaging) return
 
-    Notification.requestPermission().then((res) => {
-      console.log('🔔 permission:', res)
-    })
+      const permission = await Notification.requestPermission()
+
+      console.log('🔔 permission:', permission)
+
+      if (permission !== 'granted') return
+
+      const registration = await navigator.serviceWorker.register(
+        '/firebase-messaging-sw.js'
+      )
+
+      console.log('✅ SW registered')
+
+      const token = await getToken(messaging, {
+        vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
+        serviceWorkerRegistration: registration,
+      })
+
+      console.log('📲 FCM TOKEN:', token)
+    }
+
+    initPush()
   }, [])
 
   return null
